b8834cd9663be88871dae5eb7a3223f4da2e3949
KEVIN9112003 <147981511+KEVIN9112003@users.noreply.github.com>
2023-10-22 23:48:52 +0700

    Add files via upload

---
+++ MODUL 1/TPMODUL1_KEVIN/Account.java
@@ -0,0 +1,42 @@
+package TPMODUL1_KEVIN;
+
+
+public class Account {
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    public Account(String name, String accountNumber) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+    }
+
+    public boolean deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
